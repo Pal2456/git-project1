a89e1e309c5432e7fcb9502f49b7acf10da2123f
Pal2456 <6522772456@g.siit.tu.ac.th>
2024-11-23 22:21:57 +0700

    Reapply "caddsad"
    This reverts commit e5dc58b5e3de354be186fa5c3c3e03012718424b.

--- app/routes/connect2.js
+++ app/routes/connect2.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "roots",
-  database: "siit_library",
+  password: "root",
+  database: "siitd_library",
   /*
   host: "localhost",
   user: "root",
