21f663479ba44372159fae7c392c58a5f858ddee
Pal2456 <6522772456@g.siit.tu.ac.th>
2024-11-23 22:16:06 +0700

    dasd

--- app/routes/connect2.js
+++ app/routes/connect2.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "root",
+  password: "roots",
   database: "siit_library",
   /*
   host: "localhost",
