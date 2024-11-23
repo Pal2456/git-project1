6095c7d790966643a272ccab9e205e3e60bf7fa3
PP <6522781150@g.siit.tu.ac.th>
2024-11-23 20:55:07 +0700

    OH 600 change my gawd

--- app/app.js
+++ app/app.js
@@ -1,17 +1,31 @@
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session'); // Session middleware
+require('dotenv').config(); // Load environment variables
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set global admin property (useful for global configurations)
+app.locals.admin = process.env.ADMIN === 'true'; // Controlled by .env file
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Session setup
+app.use(session({
+  secret: 'your-secret-key', // Change this to a secure key
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,16 +33,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middleware to add admin property to each request
+app.use(function (req, res, next) {
+  req.admin = req.session.admin || app.locals.admin || false; // Flexible source
+  next();
+});
+
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
